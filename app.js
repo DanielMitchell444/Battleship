a1bbb4d09b82878deaf3b45209a3b9c07d2d16ed
Daniel Mitchell <49624246+DanielMitchell444@users.noreply.github.com>
2023-10-12 09:02:55 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+class Ship{
+ constructor(length, hits, isSunkk){
+   this.length = length;
+   this.hits = hits;
+   this.isSunkk = isSunkk;
+ }
+
+ hit(){
+    return this.hits + this.hits;
+ }
+
+ isSunk(){
+    return this.length + this.hits;
+ }
+
+ is
+}
